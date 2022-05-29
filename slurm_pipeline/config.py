8b935b5ffe9cc9884c083fabebda30759daee7e7
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-05-29 22:44:04 +0200

    Allow job properties section to be undefined

--- slurm_pipeline/config.py
+++ slurm_pipeline/config.py
@@ -207,4 +207,4 @@ def _set_defaults(config):
 
 def _merge_defaults(config):
     for job_conf in config['jobs']:
-        job_conf['properties'] = {**config['properties'], **job_conf['properties']}
+        job_conf['properties'] = {**config['properties'], **job_conf.get('properties', {})}
