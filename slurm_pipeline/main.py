a3f9c7d2fa7f5585ea3e1e406f4e9636720aeccf
fewagner <wagner@mcc-berlin.net>
2022-08-08 10:23:04 +0200

    adjust config for od_streets run

--- slurm_pipeline/main.py
+++ slurm_pipeline/main.py
@@ -13,6 +13,6 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     conf = config.load()
     logger.setLevel(conf['properties']['log_level'])
-    job_conf = config.get_job_config(conf, 'streets-downloading')
+    job_conf = config.get_job_config(conf, 'preprocessing')
     scheduler = Scheduler(job_conf)
     scheduler.main()
