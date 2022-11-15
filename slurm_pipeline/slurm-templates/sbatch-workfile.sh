356f8b58c81b4983b7a7f5f298e10750281ffbed
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-11-15 01:17:18 +0100

    Introduce maximum of concurrent processes per slurm job
    To prevent memory exhaustion, especially on the io node, which only has
    4GB of memory, at most 5 concurrent Python processes / work packages
    should be executed.
    
    Remove exit status check for backgrounded processes for now as with the
    current approach only the exit status of the last 5 Python processes is
    checked.

--- slurm_pipeline/slurm-templates/sbatch-workfile.sh
+++ slurm_pipeline/slurm-templates/sbatch-workfile.sh
@@ -4,6 +4,8 @@ set -eo pipefail
 
 pwd; hostname; date
 
+MAX_CONCURRENT_JOBS=5
+
 SCRIPT="$1"
 CONDA_ENV="$2"
 WORKFILE="$3"
@@ -18,20 +20,18 @@ if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
     jq ".[${SLURM_ARRAY_TASK_ID}]" "$WORKFILE" | python -u "$SCRIPT"
 else
     for params in $(jq -c '.[]' "$WORKFILE"); do
+
+        # process at most 5 work packages concurrently
+        until [ $(jobs -p | wc -w) -lt $MAX_CONCURRENT_JOBS ]; do
+            sleep 2
+        done
+
         # start and background process
         python -u "$SCRIPT" <<< $params &
     done
 
-    FAIL=0
     # wait for all backgrounded processes to finish
-    for job in $(jobs -p); do
-        wait $job || let "FAIL+=1"
-    done
-
-    if [ "$FAIL" != "0" ]; then
-        echo "${FAIL} work packages failed."
-        exit 1
-    fi
+    wait
 fi
 
 # SCRIPT MUSS ACCEPT INPUT FROM STDIN LIKE:
